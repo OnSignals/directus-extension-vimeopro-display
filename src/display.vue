c86def37de41703bac02e8ed2fde9d54ec1f219b
Martin Wecke <mail@hatsumatsu.de>
2023-09-20 11:53:42 +0200

    initial commit

---
+++ src/display.vue
@@ -0,0 +1,53 @@
+<template>
+  <span class="wrapper">
+    <img :src="thumbnailSrc" class="image" v-if="thumbnailSrc" />
+    <span class="title">{{ name }}</span>
+  </span>
+</template>
+
+<script>
+export default {
+  props: {
+    value: {
+      type: String,
+      default: null,
+    },
+  },
+  computed: {
+    name() {
+      return this.value?.name || null;
+    },
+
+    thumbnailSrc() {
+      return this.value?.pictures?.sizes?.[0]?.link || null;
+    },
+  },
+};
+</script>
+
+<style scoped>
+.wrapper {
+  display: inline-block;
+  height: 100%;
+}
+
+.image {
+  display: inline-block;
+  vertical-align: middle;
+
+  height: 100%;
+  aspect-ratio: 1;
+  margin: 0 12px 0 0;
+  object-fit: cover;
+
+  border-radius: var(--border-radius);
+}
+
+.title {
+  max-width: 8em;
+  overflow: hidden;
+  white-space: nowrap;
+  text-overflow: ellipsis;
+  color: var(--foreground-subdued);
+}
+</style>
---
+++ src/index.js
@@ -0,0 +1,11 @@
+import DisplayComponent from "./display.vue";
+
+export default {
+  id: "vimeopro-display",
+  name: "Vimeo Pro",
+  icon: "subscriptions",
+  description: "Display data from the Vimeo Pro interface extension",
+  component: DisplayComponent,
+  options: null,
+  types: ["json"],
+};
